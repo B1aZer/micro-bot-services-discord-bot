f84ac36f988276accdae3b2ba8b11c3c3357fc13
concave-dash <kwerkee12@gmail.com>
2022-04-07 12:48:01 -0400

    roles template

---
+++ utils/roles.js
@@ -0,0 +1,12 @@
+// Assign initial role
+// Assing roles based on XP
+module.exports = {
+    assign: (message, args, client, command, _Discord) => {
+        //let currentRole = client.user.role;
+        //let xpRole = getRoleBasedOn(xp)
+        // if (currentRole !== xpRole) {
+            // assignRole(user, xpRole)
+        //}
+        return true;
+    }
+}
