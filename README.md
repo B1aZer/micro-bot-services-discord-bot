e96a985806e6d238a4594d7e795812f42ca61e6b
concave-dash <kwerkee12@gmail.com>
2022-04-07 12:45:31 -0400

    task review and readme update

--- README.md
+++ README.md
@@ -5,10 +5,11 @@
 - [ ] permissions.js serverId move to .env
 - [ ] messageCreate.js mpve prefix to .env
 - [ ] help js admin
+- [ ] answer 6 horss
 
 #### Goals
 
-- [ ] answer
+- [x] answer
 - [ ] xp system
 - [ ] leaderboard
 - [ ] profile xp
--- commands/answer.js
+++ commands/answer.js
@@ -7,34 +7,54 @@ module.exports = {
     name: 'answer',
     aliases: ['aw'],
     description: 'Answer the task to gain XP.',
-    async execute(message, args, client, _Discord) {
-        if (!args[0]) return message.channel.send('Please enter a task ID');
-        if (!args[1]) return message.channel.send('Please provide an answer');
+    async execute(message, args, client, command, _Discord) {
+        if (!args[0]) {
+            message.channel.send('Please enter a task ID');
+            return;
+        }
+        if (!args[1]) {
+            message.channel.send('Please provide an answer');
+            return;
+        }
         try {
             let task = await taskModel.findById(args[0]);
             if (task.answer.toLowerCase() === args[1].toLowerCase()) {
-                const embed = new _Discord.MessageEmbed()
-                    .setColor("#00936f")
-                    .setTitle(message.author.username)
-                    //.setDescription("CONGRATS")
-                    .setThumbnail(message.author.displayAvatarURL())
-                    .addFields(
-                        //{ name: 'ID', value: task._id },
-                        { name: 'CONGRATS', value: `You gained ${task.xp} XP!\n\u200b` },
-                        //{ name: '\u200B', value: '' },
-                    )
-                    .setFooter({ text: message.guild.name })
-                message.channel.send({content: `<@${message.author.id}>`, embeds: [embed] })
+                if (task.review) {
+                    const embed = new _Discord.MessageEmbed()
+                        .setColor("#00936f")
+                        .setTitle(message.author.username)
+                        .setThumbnail(message.author.displayAvatarURL())
+                        .addFields(
+                            { name: 'NEEDS REVIEW', value: `Thank you!\nWe are notified and let you know.\n\u200b` },
+                        )
+                        .setTimestamp()
+                        .setFooter({ text: message.guild.name })
+                    // TODO: NOTIFY
+                    message.channel.send({ content: `<@${message.author.id}>`, embeds: [embed] })
+                } else {
+                    const embed = new _Discord.MessageEmbed()
+                        .setColor("#00936f")
+                        .setTitle(message.author.username)
+                        .setThumbnail(message.author.displayAvatarURL())
+                        .addFields(
+                            { name: 'CONGRATS', value: `You gained ${task.xp} XP!\n\u200b` },
+                        )
+                        .setTimestamp()
+                        .setFooter({ text: message.guild.name })
+                    // TODO: ADD XP
+                    message.channel.send({ content: `<@${message.author.id}>`, embeds: [embed] })
+                }
             } else {
                 const embed = new _Discord.MessageEmbed()
                     .setColor("#af0303")
                     .setTitle(message.author.username)
                     .setThumbnail(message.author.displayAvatarURL())
                     .addFields(
-                        { name: 'SORRY', value: `This is not the right answer!\nPlease try again in 6 hours.` }
+                        { name: 'SORRY', value: `This is not the right answer!\nPlease try again in 6 hours.\n\u200b` }
                     )
+                    .setTimestamp()
                     .setFooter({ text: message.guild.name })
-                message.channel.send({content: `<@${message.author.id}>`, embeds: [embed] })
+                message.channel.send({ content: `<@${message.author.id}>`, embeds: [embed] })
             }
         } catch {
             message.channel.send('Not a valid task ID');
