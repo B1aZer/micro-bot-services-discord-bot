1386486a92ab7ad21564dbadc5132ccf6468376c
concave-dash <kwerkee12@gmail.com>
2022-04-09 16:41:52 -0400

    role assign

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -1,9 +1,11 @@
 const { message } = require("discord.js");
 const permissions = require("../../utils/permissions")
 const cooldowns = require("../../utils/cooldowns")
+const profiles = require("../../utils/profiles")
+const roles = require("../../utils/roles")
 const prefix = '!';
 
-module.exports = (Discord, client, message) => {
+module.exports = async (Discord, client, message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
   const args = message.content.slice(prefix.length).split(/ +/);
   const cmd = args.shift().toLowerCase();
@@ -11,15 +13,28 @@ module.exports = (Discord, client, message) => {
   const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.includes(cmd));
   // NO COMMAND FOUND
   if (!command) return;
+  // PROFILE
+  // TODO: await, do we need it?
+  let profileData = await profiles.get(message, args, client, command, Discord);
+  if (!profileData) return;
   // PERMISSIONS
-  if (!permissions.check(message, command, client)) return;
+  // TODO: not used, remove
+  if (!permissions.check(message, args, client, command, Discord)) return;
+  // COMMAND XP VISIBLITY CHECK
+  if (!roles.check(message, args, client, command, Discord, profileData)) return;
   // COOLDOWNS
-  if (!cooldowns.set(message, command, Discord)) return;
-  // EXECUTE
+  if (!cooldowns.set(message, args, client, command, Discord)) return;
   try {
-    command.execute(message, args, client, Discord);
+    // ROLES AND LEVELING SYSTEM
+    roles.pre(message, args, client, command, Discord, profileData)
+    // EXECUTE
+    command.execute(message, args, client, command, Discord, profileData)
+    // ROLES AND LEVELING SYSTEM
+    roles.assign(message, args, client, command, Discord, profileData)
+    //DELETE
+    //message.delete();
   } catch (err) {
-    message.reply("There was an error trying execute this command!");
-    console.log(err);
+    message.reply("There was an error trying execute this command!")
+    console.log(err)
   }
 }
--- utils/roles.js
+++ utils/roles.js
@@ -1,45 +1,64 @@
 // Assign initial role
 // Assing roles based on XP
+const xp = require("../utils/xp");
+const config = require("../config");
+
 let rolesArray = [
-    ['Newcomer',       100],
-    ['Newbie',         200],
-    ['Noob',           300],
-    ['Beginner',       400],
-    ['Amateur',        500],
-    ['Trainee',        600],
-    ['Novice',         700],
-    ['Rookie',         800],
-    ['Student',        900],
-    ['Apprentice',     1000],
-    ['Accustomed',     1200],
-    ['Experienced',    1400],
-    ['Competent',      1600],
-    ['Skillful',       1800],
-    ['Expert',         2000],
-    ['Intellect',      2200],
-    ['Prodigy',        2400],
-    ['Professional',   2600],
-    ['Master',         2800],
-    ['Indestructible', 3000],
-    ['Legendary',      4000],
-    ['Mythical',       5000],
-    //['Elder',          0],
-    ['Legend',         10000],
+    ['NE0',            10000000],
     ['Tester',         1000000],
+    ['Legend',         10000],
+    //['Elder',          0],
+    ['Mythical',       5000],
+    ['Legendary',      4000],
+    ['Indestructible', 3000],
+    ['Master',         2800],
+    ['Professional',   2600],
+    ['Prodigy',        2400],
+    ['Intellect',      2200],
+    ['Expert',         2000],
+    ['Skillful',       1800],  
+    ['Competent',      1600],
+    ['Experienced',    1400],
+    ['Accustomed',     1200],
+    ['Apprentice',     1000],
+    ['Student',        900],
+    ['Rookie',         800],
+    ['Novice',         700],
+    ['Trainee',        600],
+    ['Amateur',        500],
+    ['Beginner',       400],
+    ['Noob',           300],
+    ['Newbie',         200],
+    ['Newcomer',       100],
+    ['Dummy',          0],
 ]
+
 module.exports = {
-    assign: (message, args, client, command, _Discord) => {
-        //let currentRole = client.user.role;
-        //let xpRole = getRoleBasedOn(xp)
-        // if (currentRole !== xpRole) {
-            // assignRole(user, xpRole)
+    pre: async (message, args, client, command, _Discord, profileData) => {
+        //if (profileData.xp < 100) {
+            //await xp.give(message, [100], client, _Discord, profileData);
+            //message.channel.send('Congrats!');
         //}
-//         var role = message.guild.roles.find(role => role.name === "MyRole");
-// message.member.addRole(role);
+    },
+    assign: (message, args, client, command, _Discord, profileData) => {       
+        let roleName = module.exports.meetsRole(profileData.xp);
+        config.log && console.log(`Assigning ${profileData.userID} user ${roleName} role based on ${profileData.xp} xp`);
+        let role = message.member.guild.roles.cache.find(role => role.name === roleName);
+        if (!message.member.roles.cache.some(role => role.name === roleName)) {
+            config.log && console.log(`Assign role`);
+            message.member.roles.add(role);
+        }
         return true;
     },
+    roleXP: (role) => {
+        return (rolesArray.find((roleTup) => role === roleTup[0]))[1];
+    },
+    meetsRole: (xpVal) => {
+        return (rolesArray.find((roleTup) => xpVal >= roleTup[1]))[0];
+    },
     check: (message, args, client, command, _Discord, profileData) => {
-        if (!command.xp || command.xp <= profileData.xp) {
+        // CHECK COMMAND VISIBILITY BY ROLE
+        if (!command.role || module.exports.roleXP(command.role) <= profileData.xp) {
             return true;
         }
         return false;
