ac694a05d164e3cc3a0e0d4e9d543343569611c7
concave-dash <kwerkee12@gmail.com>
2022-04-16 19:06:46 -0400

    help debug

--- commands/help.js
+++ commands/help.js
@@ -9,10 +9,13 @@ module.exports = {
     cooldown: 10,
     async execute(message, args, client, command, _Discord, profileData) {
         command_files = fs.readdirSync('./commands').filter(file => file.endsWith('js') && file !== path.basename(__filename));
+        console.info(command_files);
         //TODO: not admin commands
-        message.channel.send(`**HELP IS HERE**`);
+        message.channel.send(`**HELP IS HERE**`);    
         for(const file of command_files) {
+          // TODO: this is slow. why?
           const command = require(`./${file}`);
+          console.info(command);
           // XP CHECK
           if (roles.check(message, args, client, command, _Discord, profileData)) {
             await message.channel.send(`**${command.name}** \nAliases: ${command.aliases} \n${command.description}\nArguments: \nUsage: `);
