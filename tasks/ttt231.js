39e0e441be9262fc12c3b7723aedee9e62e30550
concave-dash <kwerkee12@gmail.com>
2022-04-17 23:35:09 -0400

    test task

---
+++ tasks/ttt231.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "ttt231",
+    category: "marketing",
+    role: "Tester",
+    xp: 10,
+    embed: `
+        # Markdown Text 101
+
+        Want to inject some flavor into your everyday text chat? You're in luck! Discord uses Markdown, a simple plain text formatting system that'll help you **make your sentences stand out**. Here's how to do it! Just add a few characters before & after your desired text to change your text! I'll show you some examples...
+
+        ## Sweet Styles
+
+        `,
+    answer: (tweet) => {
+        // give xp
+        return true;
+    }
+}
