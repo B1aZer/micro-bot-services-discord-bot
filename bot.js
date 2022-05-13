dd2a9a443530a2d2121f03d39d512882c8533bab
concave-dash <kwerkee12@gmail.com>
2022-05-12 23:39:06 -0400

    refactor

--- bot.js
+++ bot.js
@@ -2,34 +2,11 @@ require('dotenv').config();
 const Discord = require("discord.js");
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"], partials: ["CHANNEL"] })
 
-// opensea monitor
-const WebSocket = require('ws');
-const ws = new WebSocket('ws://localhost:3080');
-let ready = false;
-ws.on('message', function message(jsonData) {
-  const data = JSON.parse(jsonData)
-  if (data && ready) {
-    const embed = new Discord.MessageEmbed()
-        .setColor(data.below ? "#932f00" : "#00936f")
-        .setTitle(data.below ? 'Listed BELOW floor price' : 'Listed on floor price')
-        .setDescription('New floor listing on OpenSea.')
-        .setAuthor({ name: 'GooDeeBot', iconURL: 'https://i.imgur.com/8nB0tI0.jpg' })
-        .setURL(data.permalink)
-        .setImage(data.image)
-        .addField(data.collection, `ETH price: ${data.eth_price}\r\nFloor price: ${data.floor_price}\r\nUSD price: ${data.usd_price}\r\n${data.permalink}`)
-        .setTimestamp()
-    client.channels.cache.get('973062867647627264').send({ embeds: [embed] });
-  }
-});
-client.on('ready', client => {
-  ready = true;
-})
-//eof  opensea monitor
 
 client.commands = new Discord.Collection();
 
 // TODO: task handler, make tasks commands?
-['command_handler', 'event_handler'].forEach(handler => {
+['opensea_handler', 'command_handler', 'event_handler'].forEach(handler => {
   require(`./handlers/${handler}`)(client);
 });
 
---
+++ handlers/opensea_handler.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const WebSocket = require('ws');
+const Discord = require("discord.js");
+
+module.exports = (client) => {
+    const ws = new WebSocket('ws://localhost:3080');
+    let ready = false;
+    
+    ws.on('message', function message(jsonData) {
+        const data = JSON.parse(jsonData)
+        if (data && ready) {
+            const embed = new Discord.MessageEmbed()
+                .setColor(data.below ? "#932f00" : "#00936f")
+                .setTitle(data.below ? 'Listed BELOW floor price' : 'Listed on floor price')
+                .setDescription('New floor listing on OpenSea.')
+                .setAuthor({ name: 'GooDeeBot', iconURL: 'https://i.imgur.com/8nB0tI0.jpg' })
+                .setURL(data.permalink)
+                .setImage(data.image)
+                .addField(data.collection, `ETH price: ${data.eth_price}\r\nFloor price: ${data.floor_price}\r\nUSD price: ${data.usd_price}\r\n${data.permalink}`)
+                .setTimestamp()
+            client.channels.cache.get(process.env.OPENSEA_MONITOR_ID).send({ embeds: [embed] });
+        }
+    });
+    client.on('ready', client => {
+        ready = true;
+    })
+}
