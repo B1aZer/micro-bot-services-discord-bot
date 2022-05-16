84d72743d270dc8376024edd4b560b1f8536e1ef
concave-dash <kwerkee12@gmail.com>
2022-05-15 22:45:25 -0400

    enter command

---
+++ commands/enter.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	cooldown: 10,
+	data: new SlashCommandBuilder()
+		.setName('enter')
+		.setDescription('Enter server.')
+		.addStringOption(option =>
+			option.setName('password')
+				.setDescription('Password to enter server.')
+				.setRequired(true))
+	,
+	async execute(interaction) {
+		const password = interaction.options.getString('password');
+		if (password === process.env.ENTER_PASSWORD) {
+			const allRoles = interaction.client.roles.sort((a,b) => a.rawPosition - b.rawPosition)
+			const firstRole = [...allRoles][1];
+			interaction.member.roles.add(firstRole);
+			await interaction.reply({content: ':tada: Congrats!', ephemeral: true });
+		} else {
+			await interaction.reply({content: 'This is not the right password', ephemeral: true });
+		}	
+	},
+};
