e5deb4e6ec947ac20a8dfa259d247e77ddc3166a
concave-dash <kwerkee12@gmail.com>
2022-05-19 00:46:13 -0400

    eth handler

--- handlers/ethereum_handler.js
+++ handlers/ethereum_handler.js
@@ -1,8 +1,10 @@
 const { ethers } = require("ethers");
 const fs = require('fs');
+const axios = require('axios')
 const logger = fs.createWriteStream(`/home/hipi/Sites/GooDee/_utils/out/coins/main.log`, {
     flags: 'a'
 });
+const { memberNicknameMention } = require('@discordjs/builders');
 
 const coinPrice = 0.00033;
 const contractAddress = '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0';
@@ -116,9 +118,34 @@ module.exports = async (client) => {
     const provider = new ethers.providers.JsonRpcProvider();
     const gooDeeContract = new ethers.Contract(contractAddress, abi, provider);
     // Receive an event when ANY transfer occurs
-    gooDeeContract.on("buyCoinsEvent", (from, discordID, amount, event) => {
-        console.log(`${from} sent ${ethers.utils.formatEther(amount)} to ${discordID}`);
-        logger.write(`${event.transactionHash} ${process.env.LOG_FILES_SEPARATOR} ${discordID} ${process.env.LOG_FILES_SEPARATOR} ${from} ${process.env.LOG_FILES_SEPARATOR} ${ethers.utils.formatEther(amount)}\r\n`)
+    gooDeeContract.on("buyCoinsEvent", async (from, discordID, amount, event) => {
+        // event params are BigNumber's
+        // log
+        console.log(`${from.toString()} sent ${ethers.utils.formatEther(amount)} to ${discordID.toString()}`);
+        // get user
+        const userDB = (await axios.get(`${process.env.MONGODB_URL}/user`, { params: { userID: discordID.toString() } })).data
+        // if tx in db do nothing
+        if (userDB?.txs?.indexOf(event.transactionHash.toString()) >= 0) return; 
+        // log
+        logger.write(`${event.transactionHash.toString()} ${process.env.LOG_FILES_SEPARATOR} ${discordID.toString()} ${process.env.LOG_FILES_SEPARATOR} ${from.toString()} ${process.env.LOG_FILES_SEPARATOR} ${ethers.utils.formatEther(amount)}\r\n`)
+        // not found
+        if (!userDB) {
+            console.log(`No user found to add coins to ${discordID.toString()}`)
+            return;
+        }
+        // calculate coins from BigNumber's
+        const numberOfCoins = (amount.div(ethers.BigNumber.from(ethers.utils.parseEther(coinPrice.toString())))).toNumber()
+        // save coins and txs
+        await axios.put(`${process.env.MONGODB_URL}/user`, {
+            userID: discordID.toString(),
+            coins: userDB.coins + numberOfCoins,
+            txs: [event.transactionHash.toString()]
+        })
+        // message
+        // TODO: will not work until ready
+        //const channel = client.channels.cache.get(process.env.LOG_CHANNEL_ID);
+        //channel?.send(`${memberNicknameMention(discordID.toString())} received ${numberOfCoins} coins`);
+        console.log(`${discordID.toString()} received ${numberOfCoins} coins`);
     });
 
 }
