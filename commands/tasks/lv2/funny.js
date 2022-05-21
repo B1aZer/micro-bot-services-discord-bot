722fc3e299d16c1c264349432f34602aa0dcd384
concave-dash <kwerkee12@gmail.com>
2022-05-21 15:24:29 -0400

    require base channel for funny command

--- commands/tasks/lv2/funny.js
+++ commands/tasks/lv2/funny.js
@@ -1,4 +1,4 @@
 const data = require('../data/lv2.json')[0];
-const taskBase = require('../base/taskBase');
+const taskBase = require('../base/taskChannelBase');
 
 module.exports = new taskBase(data);
