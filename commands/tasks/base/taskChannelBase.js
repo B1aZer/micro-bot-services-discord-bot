a71fc8c2a846a8e48024ace93564bbccc0e9c8cc
concave-dash <kwerkee12@gmail.com>
2022-05-21 15:23:56 -0400

    base chack for channelBase class

--- commands/tasks/base/taskChannelBase.js
+++ commands/tasks/base/taskChannelBase.js
@@ -45,8 +45,21 @@ module.exports = class TaskChannelBase extends TaskBase {
     }
     async getMessageFrom(answer, interaction) {
         const messId = answer.split('/')[answer.split('/').length - 1];
-        const channel = await interaction.client.channels.fetch(process.env.MOTIVATION_CHANNEL_ID);
+        const channel = await interaction.client.channels.fetch(process.env[this.channelEntryId]);
         const message = await channel.messages.fetch(messId);
         return message;
+    }
+    async checkIfErr(answer, interaction) {
+        try {
+            console.log(`checking user answer ${answer}`);
+            const message = await this.getMessageFrom(answer, interaction);
+            if (message.author.id !== interaction.member.id) {
+                return `This is not yours message!`;
+            }
+            return false;
+        } catch (err) {
+            console.log(err);
+            return `That's not a valid Message Link.`;
+        }
     }
 }
