1e9f413bf174a608dda2a79944ea613fb405c7b5
concave-dash <kwerkee12@gmail.com>
2022-05-21 23:22:29 -0400

    add lv3 reward message

--- utils/data/lvData.js
+++ utils/data/lvData.js
@@ -1,5 +1,6 @@
 const lv1Tasks = require('../../commands/tasks/data/lv1.json');
 const lv2Tasks = require('../../commands/tasks/data/lv2.json');
+const lv3Tasks = require('../../commands/tasks/data/lv3.json');
 
 function reduceTasks(tasks) {
     return tasks.reduce((prev, curr) => prev + (curr.submitionsAllowed || 1), 0);
@@ -15,6 +16,11 @@ module.exports = {
     2: {
         tasksNum: reduceTasks(lv2Tasks),
         unlocked: '/funny command',
+        coins: 100,
+    },
+    3: {
+        tasksNum: reduceTasks(lv3Tasks),
+        unlocked: '/motivate command',
         coins: 100,
     },
 }
